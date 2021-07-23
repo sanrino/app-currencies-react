d944168f5f8e7c7d30a4cba39cd4b66f3270ee03
YevheniiaYelsukova <yevheniia.yelsukova@faceit.com.ua>
2021-07-23 12:24:16 +0300

    fixed: chart.js

--- src/components/ExchangeRateDetail/ExchangeRateDetailChart.jsx
+++ src/components/ExchangeRateDetail/ExchangeRateDetailChart.jsx
@@ -1,35 +1,65 @@
-import React, { useEffect, useRef, useState } from "react";
-import uniqId from "uniqid";
+import React, { useEffect, useState, useRef } from "react";
 
-import { Line } from "react-chartjs-2";
+import { Chart, registerables } from 'chart.js';
+import annotationPlugin from 'chartjs-plugin-annotation';
+
+Chart.register(...registerables);
+Chart.register(annotationPlugin);
 
 export const ExchangeRateDetailChart = ({ pointsData }) => {
-  const [chartData, setChartData] = useState({});
 
   const dates = pointsData.map(({ exchangedate }) => exchangedate);
   const rates = pointsData.map(({ rate }) => rate);
 
-  const chart = () => {
-    setChartData({
-      labels: dates,
-      datasets: [
-        {
-          label: [pointsData[0].cc],
-          data: rates,
-          backgroundColor: "rgba(75,192,192,0.6)",
-          borderWidth: 4,
-        },
-      ],
-    });
-  };
+  const chartCanvasRef = useRef(null);
+
+  const [isRebuildingCanvas, setIsRebuildingCanvas] = useState(false);
 
   useEffect(() => {
-    chart();
+    setIsRebuildingCanvas(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pointsData]);
 
+  useEffect(() => {
+    if (isRebuildingCanvas) {
+      setIsRebuildingCanvas(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isRebuildingCanvas]);
+
+  useEffect(() => {
+    const chartCanvas = chartCanvasRef.current
+
+    if (isRebuildingCanvas || !chartCanvas) {
+      return;
+    }
+
+    const chartInstance = new Chart(chartCanvasRef.current, {
+      type: "line",
+      data: {
+        labels: dates,
+        datasets: [
+          {
+            label: pointsData[0].cc,
+            data: rates,
+            backgroundColor: "rgba(75,192,192,0.6)",
+            borderWidth: 4,
+          },
+        ],
+      },
+    });
+
+    return () => {
+      chartInstance.destroy();
+    }
+
+  }, [isRebuildingCanvas, pointsData]);
+
   return (
-    <div>
-      {<Line data={chartData} type="line" id={uniqId()} key={uniqId()} />}
+    <div className="chart">
+      {isRebuildingCanvas ? undefined : (
+        <canvas ref={chartCanvasRef} />
+      )}
     </div>
   );
 };
